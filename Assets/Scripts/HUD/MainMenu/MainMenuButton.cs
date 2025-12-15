4449a165d89b215c966c154da2cbd6bdb23293e4
Kilian Monnier <kokoshibo524@gmail.com>
2025-12-15 12:45:38 +0100

    feat-HUD
    MainMenu 75% complete and InGame UI 50% complete

--- Assets/Scripts/HUD/MainMenu/MainMenuButton.cs
+++ Assets/Scripts/HUD/MainMenu/MainMenuButton.cs
@@ -1,10 +1,21 @@
+using EasyTextEffects;
 using UnityEngine;
 
 public class MainMenuButton : MonoBehaviour {
     public CanvasGroup canvasGroup;
     public float fadeDuration;
+    
+    [SerializeField] private TextEffect textEffect;
 
     public void OpenCanvas() {
         MainMenuTestManager.Instance.OnOpenClosePanel(canvasGroup, 1, fadeDuration, true);
+    }
+
+    public void UseEffect(string _effectName) {
+        textEffect.StartManualEffect(_effectName);
+    }
+    
+    public void StopEffects() {
+        textEffect.StopAllEffects();
     }
 }
